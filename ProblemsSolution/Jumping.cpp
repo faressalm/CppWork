3bbf2e04264f740279bd670a6f79df20298ef127
Fares Waheed <faressalm44@hotmail.com>
2021-08-21 12:21:42 +0200

    JUmping

---
+++ ProblemsSolution/Jumping.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+#define PB push_back
+#define loop(n)     for(int i=1;i<=(n);i++)
+#define loopzero(n)     for(int j=0;j<(n);j++)
+#define MP          make_pair
+#define all(a)      a.begin(), a.end()
+#define MAXIN       1000000
+#define clr(x,val)  memset(x,val,sizeof(x))     
+#define fast cin.sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main(){
+    fast;
+    
+
+    return 0;
+}

d43325fbdeeb9a192441e822725f9c3bf58ce34c
Fares Waheed <faressalm44@hotmail.com>
2021-08-21 18:59:01 +0200

    accepted jumping

--- ProblemsSolution/Jumping.cpp
+++ ProblemsSolution/Jumping.cpp
@@ -13,9 +13,49 @@ typedef vector<int> vi;
 #define clr(x,val)  memset(x,val,sizeof(x))     
 #define fast cin.sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define endl '\n'
+ vi bfs( vector<vi> shops, int s) {
+  vi distances(shops.size()+1,-1);
+  distances[s]=0;
+  queue<int> q;
+  q.push(s);
+  while(!q.empty()){
+       int node = q.front();   q.pop();
+        int cost = distances[node];
+
+        for(int child: shops[node])     if(distances[child]== -1)
+        {
+            q.push(child);
+            distances[child] = cost+1;
+        }
+
+  }
+    
+  return distances;
+}
 
 int main(){
     fast;
+    int tests;
+    cin>>tests;
+    while (tests--)
+    {
+       int N;
+       cin>>N;
+       vector<vi> shops (N+1);
+       vi distances(N+1); 
+       loop(N){
+         int p;
+         cin>>p;
+         int u = i+p, v = i-p;
+         if(v>1)
+         shops[i].PB(v);
+         if(u<N)
+         shops[i].PB(u);
+       }
+       distances=bfs(shops,N);
+       for(int j=1;j<=N;j++)
+       cout<<distances[j]<<endl;
+    }
     
 
     return 0;
